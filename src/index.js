0563522f9afd81246b98f1dce9ccee37631b32b0
LenaChu <a87192611@gmail.com>
2021-02-25 22:22:58 -0600

    adjusted responsive code

--- src/index.js
+++ src/index.js
@@ -5,16 +5,16 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import iTunesReducer from "./reducer/Reducer";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 let store = createStore(iTunesReducer);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <Router>
         <App />
-      </BrowserRouter>
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
